aadc35539aa15122bba4236a5e2c3d2558561eee
carey <schiafang@gmail.com>
2020-05-05 22:12:09 +0800

    v1

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
+const generator = require('./generator.js')
+const app = express()
+const port = 3000
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
+app.post('/', (req, res) => {
+  const career = req.body.options
+  const results = generator(career)
+  res.render('index', { results })
+})
+
+app.listen(port, () => {
+  console.log(`the server is listening on http://localhost:${port}`)
+})
---
+++ generator.js
@@ -0,0 +1,27 @@
+function generator (career) {
+  const task = {
+    engineer: ['加個按鈕', '加新功能', '切個版', '改一點 code'],
+    designer: ['畫一張圖', '改個 logo', '順便幫忙設計一下', '隨便換個設計'],
+    entrepreneur: ['週末加班', '要能賺錢', '想個 business model', '找 VC 募錢']
+  }
+  const phrase = ['很簡單', '很容易', '很快', '很正常']
+  let result = []
+
+  function randomIndex (option) {
+    return Math.floor(Math.random() * option.length)
+  }
+
+  if (career === 'engineer') {
+    result = task.engineer[randomIndex(task.engineer)] + phrase[randomIndex(phrase)]
+  }
+  if (career === 'designer') {
+    result = task.designer[randomIndex(task.designer)] + phrase[randomIndex(phrase)]
+  }
+  if (career === 'entrepreneur') {
+    result = task.entrepreneur[randomIndex(task.entrepreneur)] + phrase[randomIndex(phrase)]
+  }
+
+  return result
+}
+
+module.exports = generator
