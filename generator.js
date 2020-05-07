030f6d6d88dd59cdba5391ea913852733e634d26
carey <schiafang@gmail.com>
2020-05-07 10:17:04 +0800

    revise

--- generator.js
+++ generator.js
@@ -4,6 +4,7 @@ function generator (career) {
     designer: ['畫一張圖', '改個 logo ', '順便幫忙設計一下', '隨便換個設計'],
     entrepreneur: ['週末加班', '要能賺錢', '想個 business model ', '找 VC 募錢']
   }
+  const careers = ['工程師', '設計師', '創業家']
   const phrase = ['很簡單', '很容易', '很快', '很正常']
   let result = []
 
@@ -11,16 +12,12 @@ function generator (career) {
     return Math.floor(Math.random() * option.length)
   }
 
-  if (career === 'engineer') {
-    result = '身為一個工程師，' + task.engineer[randomIndex(task.engineer)] + phrase[randomIndex(phrase)] + '吧！'
+  for (let i = 0; i < careers.length; i++) {
+    if (career === Object.keys(task)[i]) {
+      const key = Object.keys(task)[i]
+      result = '身為一個' + careers[i] + '，' + task[key][i] + phrase[randomIndex(phrase)] + '吧！'
+    }
   }
-  if (career === 'designer') {
-    result = '身為一個設計師，' + task.designer[randomIndex(task.designer)] + phrase[randomIndex(phrase)] + '吧！'
-  }
-  if (career === 'entrepreneur') {
-    result = '身為一個創業家，' + task.entrepreneur[randomIndex(task.entrepreneur)] + phrase[randomIndex(phrase)] + '吧！'
-  }
-
   if (result.length === 0) { return '點選一個 你想對誰說幹話～' }
   else { return result }
 }
